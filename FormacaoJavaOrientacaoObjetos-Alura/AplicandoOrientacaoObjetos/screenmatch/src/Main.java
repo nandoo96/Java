1dae4856966addab2f50e94bb13d92c8687076c2
Fernando Silva <nando96sj@gmail.com>
2023-10-03 13:02:31 -0300

    Reaproveitando características e comportamentos - Herança e Polimorfismo

--- FormacaoJavaOrientacaoObjetos-Alura/AplicandoOrientacaoObjetos/screenmatch/src/Main.java
+++ FormacaoJavaOrientacaoObjetos-Alura/AplicandoOrientacaoObjetos/screenmatch/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +8,7 @@ public class Main {
         meuFilme.setNome("O Poderoso Chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do Filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -13,5 +16,25 @@ public class Main {
         meuFilme.avalia(10);
         System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("Média das Avaliações: " + meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
     }
 }
