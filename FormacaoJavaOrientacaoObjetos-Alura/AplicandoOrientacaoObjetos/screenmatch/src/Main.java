47527461eaa923cbed680ed7298ec1e53e11a93c
Fernando Silva <nando96sj@gmail.com>
2023-10-03 11:16:56 -0300

    Controlando o acesso e escrita nos dados - Encapsulamento

--- FormacaoJavaOrientacaoObjetos-Alura/AplicandoOrientacaoObjetos/screenmatch/src/Main.java
+++ FormacaoJavaOrientacaoObjetos-Alura/AplicandoOrientacaoObjetos/screenmatch/src/Main.java
@@ -1,17 +1,17 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-
-        System.out.println("\nSoma das Avaliações: " + meuFilme.somaDasAvaliacoes);
-        System.out.println("Total de Avaliações: " + meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("Média das Avaliações: " + meuFilme.pegaMedia());
     }
 }
